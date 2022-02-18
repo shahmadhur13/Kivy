1aae50eb5a499283c1b733dffe6c3881835f4a5c
Madhur Shah <madhurpython@gmail.com>
2022-02-18 12:21:04 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.metrics import dp
+
+
+class MyApp(App):
+    pass
+
+
+class Board(GridLayout):
+    pass
+
+
+MyApp().run()
